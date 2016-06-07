c28ea89f70b35db91f39e3d39fa44d7695e11f3b
Bill Miller <Bill.Miller@marklogic.com>
2016-06-06 20:05:56 -0400

    Adding Promises
    Added promises to use instead of callbacks (in most cases)

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,3 +1,4 @@
+var Promise = require('bluebird');
 var common = require('../lib/common.js');
 var conflate = require('conflate');
 var fs = require('fs');
@@ -6,7 +7,8 @@ var marklogic = require('marklogic');
 var util = require('../lib/utils.js');
 var logger = util.consoleLogger;
 
-var DBManager = function(env) {
+var DBManager = function(env){
+
     if(!(this instanceof DBManager)) {
         return new DBManager(env);
     }
@@ -77,7 +79,7 @@ DBManager.prototype.getConfiguration = function(type) {
         //merge lists and remove duplicates
         var defs = baseDefs.concat(envDefs);
         defs = defs.filter(function(elem, pos) {
-            return defs.indexOf(elem) == pos;
+            return defs.indexOf(elem) === pos;
         });
 
         this.configuration[type] = defs;
@@ -86,131 +88,137 @@ DBManager.prototype.getConfiguration = function(type) {
     return this.configuration[type];
 };
 
-DBManager.prototype.initializeMultiObjects = function(type, url, typeName, supported, callback) {
+DBManager.prototype.initializeMultiObjects = function(type, url, typeName, supported) {
 
     var defs = this.getConfiguration(type);
 
     var that = this;
-
-    var callBackwhenDone = (function() {
-        var total = defs.length;
-        return function() {
-            total = total-1;
-            if (total < 1) {
-                callback();
-            }
-        };
-    })();
-
-    //Initilialize all
-    defs.forEach(function(item){
-        var settings = common.objectSettings(type + '/' + item, that.env);
-        var BASE_SERVER_URL = '/manage/v2/' + url;
-        var UPDATE_SERVER_URL = BASE_SERVER_URL + '/' + settings[typeName];
-        var manager = that.getHttpManager();
-
-        //Check if exists
-        manager.get({
-            endpoint: UPDATE_SERVER_URL
-        }).
-        result(function(response) {
-            if (response.statusCode === 404) {
-                /* does not exist. Create it */
-               manager.post({
-                   endpoint : BASE_SERVER_URL,
-                   body : settings
-               })
-               .result(function(response) {
-                    if (response.statusCode === 201) {
-                        callBackwhenDone();
-                    } else {
-                        logger.error('Error when creating %s [Error %s]', item, response.statusCode);
-                        console.error(response.data);
-                        process.exit(1);
-                    }
-               });
-            } else if (response.statusCode === 200) {
-               //Already present.
-               //Sometimes only some of the properties are allowed on update. Remove non supported ones
-               //Construct payload based on supported properties
-               var payload = supported ? that.filterUnsupportedProperties(supported, settings) : settings;
-               if(keys(payload).length > 0){
-                   //There is something to send
-                   manager.put({
-                       endpoint : UPDATE_SERVER_URL + '/properties',
+    return new Promise(function(resolve, reject){
+        var callBackwhenDone = (function() {
+            var total = defs.length;
+            return function() {
+                total = total-1;
+                if (total < 1 ){
+                    resolve(type + ' Initialized');
+                }
+            };
+        })();
+        //Initilialize all
+        defs.forEach(function(item){
+            var settings = common.objectSettings(type + '/' + item, that.env);
+            var BASE_SERVER_URL = '/manage/LATEST/' + url;
+            var UPDATE_SERVER_URL = BASE_SERVER_URL + '/' + settings[typeName];
+            var manager = that.getHttpManager();
+            //Check if exists
+            manager.get({
+                endpoint: UPDATE_SERVER_URL
+            }).
+            result(function(response) {
+                if (response.statusCode === 404) {
+                    /* does not exist. Create it */
+                   manager.post({
+                       endpoint : BASE_SERVER_URL,
                        body : settings
+                   })
+                   .result(function(response) {
+                        if (response.statusCode === 201) {
+                            callBackwhenDone();
+                        } else {
+                            reject('Error when creating '+item+' [Error '+ response.statusCode +']');
+                            //logger.error('Error when creating %s [Error %s]', item, response.statusCode);
+                            console.error(response.data);
+                            //process.exit(1);
+                        }
+                   });
+                } else if (response.statusCode === 200) {
+                   //Already present.
+                   //Sometimes only some of the properties are allowed on update. Remove non supported ones
+                   //Construct payload based on supported properties
+                   var payload = supported ? that.filterUnsupportedProperties(supported, settings) : settings;
+                   if(keys(payload).length > 0){
+                       //There is something to send
+                       manager.put({
+                           endpoint : UPDATE_SERVER_URL + '/properties',
+                           body : settings
+                       })
+                       .result(function(response) {
+                            if (response.statusCode === 204) {
+                                callBackwhenDone();
+                            } else {
+                                console.error(response.data);
+                                reject('Error when updating '+item+' [Error '+ response.statusCode +']');
+                                //logger.error('Error when updating %s [Error %s]', item, response.statusCode);
+                                //process.exit(1);
+                            }
+                       });
+                   } else {
+                       //nothing to send. We are done with this
+                       callBackwhenDone();
+                   }
+                } else {
+                    console.error(response.data);
+                    reject('Error when checking '+item+' [Error '+ response.statusCode +']');
+                    //logger.error('Error when checking %s [Error %s]', item, response.statusCode);
+                    //process.exit(1);
+                }
+            });
+        });
+
+    });
+
+};
+
+DBManager.prototype.removeMultiObjects = function(type, url, typeName, params) {
+
+    var defs = this.getConfiguration(type);
+    var that = this;
+
+    return new Promise(function(resolve, reject){
+        var callBackwhenDone = (function() {
+            var total = defs.length;
+            return function() {
+                total = total-1;
+                if (total < 1) {
+                    resolve(type + ' removed');
+                }
+            };
+        })();
+        //Initilialize all
+        defs.forEach(function(item){
+            var settings = common.objectSettings(type + '/' + item, that.env);
+            var SERVER_URL = '/manage/LATEST/' + url +  '/' + settings[typeName];
+            var manager = that.getHttpManager();
+            //Check if exists
+            manager.get({
+                endpoint: SERVER_URL
+            }).
+            result(function(response) {
+                if (response.statusCode === 200) {
+                   //present.
+                   manager.remove({
+                       endpoint : SERVER_URL,
+                       params : params
                    })
                    .result(function(response) {
                         if (response.statusCode === 204) {
                             callBackwhenDone();
                         } else {
-                            logger.error('Error when updating %s [Error %s]', item, response.statusCode);
+                            reject('Error when deleting '+item+' [Error '+response.statusCode+']');
+                            //logger.error('Error when deleting %s [Error %s]', item, response.statusCode);
                             console.error(response.data);
-                            process.exit(1);
+                            //process.exit(1);
                         }
                    });
-               } else {
-                   //nothing to send. We are done with this
-                   callBackwhenDone();
-               }
-            } else {
-                logger.error('Error when checking %s [Error %s]', item, response.statusCode);
-                console.error(response.data);
-                process.exit(1);
-            }
-        });
-    });
-};
-
-DBManager.prototype.removeMultiObjects = function(type, url, typeName, params, callback) {
-
-    var defs = this.getConfiguration(type);
-
-    var that = this;
-
-    var callBackwhenDone = (function() {
-        var total = defs.length;
-        return function() {
-            total = total-1;
-            if (total < 1) {
-                callback();
-            }
-        };
-    })();
-
-    //Initilialize all
-    defs.forEach(function(item){
-        var settings = common.objectSettings(type + '/' + item, that.env);
-        var SERVER_URL = '/manage/v2/' + url +  '/' + settings[typeName];
-        var manager = that.getHttpManager();
-        //Check if exists
-        manager.get({
-            endpoint: SERVER_URL
-        }).
-        result(function(response) {
-            if (response.statusCode === 200) {
-               //present.
-               manager.remove({
-                   endpoint : SERVER_URL,
-                   params : params
-               })
-               .result(function(response) {
-                    if (response.statusCode === 204) {
-                        callBackwhenDone();
-                    } else {
-                        logger.error('Error when deleting %s [Error %s]', item, response.statusCode);
-                        console.error(response.data);
-                        process.exit(1);
-                    }
-               });
-            } else if (response.statusCode === 404) {
-                //already removed
-                callBackwhenDone();
-            } else {
-                logger.error('Error when checking %s [Error %s]', item, response.statusCode);
-                console.error(response.data);
-                process.exit(1);
-            }
+                } else if (response.statusCode === 404) {
+                    //already removed
+                    callBackwhenDone();
+                } else {
+                    reject('Error when checking '+item+' [Error '+response.statusCode+']');
+                    //logger.error('Error when checking %s [Error %s]', item, response.statusCode);
+                    console.error(response.data);
+                    //process.exit(1);
+                }
+            });
         });
     });
 };
